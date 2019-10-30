6862d1d565f01493470ae0ca6b7bee166f0ee467
Brian Bowen <babowentx@gmail.com>
2019-10-29 22:28:14 -0500

    Getting messaging working and creating list for random responses

--- bot.py
+++ bot.py
@@ -7,6 +7,15 @@ load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
+channelname = 'general'
+
+def GetReportMessage(member):
+    messages = [
+        "%s has been reported to HR." % member.mention,
+        "%s, I've set up a meeting with Jason Penn to discuss your recent behavior." % member.mention,
+        "Thank you for bringing %s to my attention." % member.mention
+    ]
+    return random.choice(messages)
 
 @client.event
 async def on_ready():
@@ -21,13 +30,24 @@ async def on_member_join(member):
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    """ print(str(channelname) + "\n")
+    print(str(message.channel.name) + "\n")
+    print(str(channelname == message.channel.name) + "\n")
+    print(str(message.author) + "\n")
+    print(str(client.user) + "\n")
+    print(str(message.author == client.user) + "\n")
+    print(str("report" in message.content or "Report" in message.content) + "\n")
+    print(str(message.content) + "\n")
+    for member in message.mentions:
+        print("Mentioned: " + str(member.name) + "\n") """
+
+    if message.author == client.user or message.channel.name != channelname:
         return
-    elif "report" in message.content:
-        await client.send_message(message.channel, "I'm trying!")
+    elif "report" in message.content or "Report" in message.content:
+#        await message.channel.send("I'm trying!")
         for member in message.mentions:
             if client.user != member:
-                await client.send_message(message.channel, "%s has been reported to HR." % member.mention)
+                await message.channel.send(GetReportMessage(member))
             else:
                 continue
 
